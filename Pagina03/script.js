13ec8c91e513e64c3f5d5aa97999df5f0a8b2fad
brunogomesss <brunoleonardogss@gmail.com>
2024-11-04 03:50:05 -0300

    commit
    commit

---
+++ Pagina03/script.js
@@ -0,0 +1,20 @@
+function trocardeCor(){
+        
+
+    if (document.body.style.backgroundColor === "#010300") {
+       
+        document.body.style.backgroundColor = "#bbdec6";
+        document.body.style.color = "#010300";
+
+
+    }
+
+    
+    else {
+       
+        document.body.style.backgroundColor = "#010300";
+        document.body.style.color = "#bbdec6";
+
+    }
+
+}
